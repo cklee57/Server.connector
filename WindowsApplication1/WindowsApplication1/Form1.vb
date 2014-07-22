a2a4bda7b74022c80f54824934bebbb9ed8610a6
cklee57 <ck_57@hotmail.com>
2014-07-22 17:55:09 +0800

    Application added

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim r1 As DialogResult = MessageBox.Show("Message with Help file.", _
+                                   "Help Caption", MessageBoxButtons.OK, _
+                                   MessageBoxIcon.Question, _
+                                   MessageBoxDefaultButton.Button1, _
+                                   0, _
+                                   "mspaint.chm")
+    End Sub
+
+
+End Class
