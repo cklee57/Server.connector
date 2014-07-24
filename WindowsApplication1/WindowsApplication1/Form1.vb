de0ea0226a74a29c6fb1e3584a82bb575b530e0a
ck57gmail <cklee57@gmail.com>
2014-07-24 17:23:14 +0800

    let do this way!!!
    ok?

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim r1 As DialogResult = MessageBox.Show("Message with Help file. yahoo", _
+        Dim r1 As DialogResult = MessageBox.Show("Message with Help file. this is stupid", _
                                    "Help Caption", MessageBoxButtons.OK, _
                                    MessageBoxIcon.Question, _
                                    MessageBoxDefaultButton.Button1, _
